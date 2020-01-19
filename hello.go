68cb9b42f23c54924430d11839a9f08a4c125bfb
zhanghuan <aystzh@163.com>
2020-01-19 15:56:05 +0800

    hellow word

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
 import "fmt"
+import "github.com/aystzh/hello/morestrings"
+import "github.com/google/go-cmp/cmp"
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+        fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
