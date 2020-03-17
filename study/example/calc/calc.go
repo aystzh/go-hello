cc45f6a792eed3e346937587f2fa915115a1ca44
zhanghuan <aystzh@163.com>
2020-03-17 17:08:12 +0800

    delete

--- study/example/calc/calc.go
+++
@@ -1,5 +0,0 @@
-package calc
-
-func Add(num1 int, num2 int) int {
-	return num1 + num2
-}
--- study/modules/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-	"github.com/aystzh/go-hello/study/modules/calc"
-)
-func main() {
-	fmt.Println(calc.Add(10, 3))
-}
